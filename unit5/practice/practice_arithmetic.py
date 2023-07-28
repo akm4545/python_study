89c4c152684d626e3ab73506933c58f402eb08b8
akm4545 <akm4545@naver.com>
2023-07-28 11:02:10 +0900

    unit5 practice

---
+++ unit5/practice/practice_arithmetic.py
@@ -0,0 +1,5 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #아파트에서 소음이 가장 심한 층수 출력하
+>>> print(int(0.2467 * 12 + 4.159))
+7
