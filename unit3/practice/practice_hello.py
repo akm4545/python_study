269dfe140ef6043112b89dbba0fb77a32ab4d482
akm4545 <akm4545@naver.com>
2023-07-28 10:19:24 +0900

    unit3 practoce

---
+++ unit3/practice/practice_hello.py
@@ -0,0 +1,7 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #'Hello, world!'와 'Python Programming'이 각 줄에 출력되게 만드세요.
+>>> print('Hello, world!')
+Hello, world!
+>>> print('Python Programming')
+Python Programming
