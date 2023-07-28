2dbb0334cf21f1896805e7e06b0532462ee6c07a
akm4545 <akm4545@naver.com>
2023-07-28 12:28:52 +0900

    unit7

---
+++ unit7/print.py
@@ -0,0 +1,28 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print(1, 2, 3)
+1 2 3
+>>> print('Hello', 'Python')
+Hello Python
+>>> print(1, 2, 3, sep=", ")
+1, 2, 3
+>>> print(4, 5, 6, sep=',')
+4,5,6
+>>> print("Hello", "Python", sep="")
+HelloPython
+>>> print(1920, 1080, sep="x")
+1920x1080
+>>> print(1, 2, 3)
+1 2 3
+>>> print(1, 2, 3, sep='\n')
+1
+2
+3
+>>> print('1\n2\m3')
+1
+2\m3
+>>> print('1\n2\n3\n')
+1
+2
+3
+
