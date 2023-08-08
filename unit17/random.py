e8c5d61c0b1222d86c7fe1d748dec7081f00d28d
akm4545 <akm4545@naver.com>
2023-08-08 23:46:28 +0900

    unit17

---
+++ unit17/random.py
@@ -0,0 +1,15 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import random
+>>> random.random()
+0.3549626665832114
+>>> random.random()
+0.7705704647213365
+>>> random.random()
+0.6279889902683274
+>>> random.randint(1, 6)
+4
+>>> random.randint(1, 6)
+1
+>>> random.randint(1, 6)
+4
