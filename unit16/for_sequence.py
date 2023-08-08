cd4eef099d82a88c2bb781dc3fdbe6d66d3a8bd6
akm4545 <akm4545@naver.com>
2023-08-08 23:13:36 +0900

    unit16

---
+++ unit16/for_sequence.py
@@ -0,0 +1,30 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = [10, 20, 30, 40, 50]
+>>> for i in a :
+...     print(i)
+... 
+...     
+10
+20
+30
+40
+50
+>>> fruits = ('apple', 'orange', 'grape')
+>>> for fruit in fruits:
+...     print(fruit)
+... 
+...     
+apple
+orange
+grape
+>>> for letter in 'Python':
+...     print(letter, end= ' ')
+... 
+...     
+P y t h o n 
+>>> for letter in reversed('Python'):
+...     print(letter, end=' ')
+... 
+...     
+n o h t y P 
