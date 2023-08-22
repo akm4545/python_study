f00ceed376ef865b34a6c32a35136d2715913a1e
akm4545 <akm4545@naver.com>
2023-08-22 17:27:49 +0900

    unit33

---
+++ unit33/namespace.py
@@ -0,0 +1,12 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x = 10
+>>> locals()
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, 'x': 10}
+>>> def foo():
+...     x = 10
+...     print(locals())
+... 
+...     
+>>> foo()
+{'x': 10}
