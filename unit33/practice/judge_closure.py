54a6a10b66544387bcb9a247ed0891f8a3554b4c
akm4545 <akm4545@naver.com>
2023-08-23 17:19:36 +0900

    unit33

--- unit33/practice/judge_closure.py
+++ unit33/practice/judge_closure.py
@@ -1,8 +1,10 @@
 def countdown(n):
-
-
-
-
+    count = n + 1
+    def minus_count():
+        nonlocal count
+        count = count - 1
+        return count
+    return minus_count
 
 n = int(input())
 
