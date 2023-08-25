1a7dc1718f21891999f7b4a5e1cdbd3ea7a43f60
akm4545 <akm4545@naver.com>
2023-08-25 11:45:37 +0900

    unit39 practice

--- unit39/practice/judge_iterator.py
+++ unit39/practice/judge_iterator.py
@@ -1,22 +1,24 @@
 class TimeIterator:
-    def __init__(self, start, stop, index):
+    def __init__(self, start, stop):
         self.start = start
         self.stop = stop
-        self.index = index
-        self.current = 0
+        self.current = start
 
     def __iter__(self):
         return self
 
-    def __next__():
-        if index < self.stop - self.start:
-            f
+    def __next__(self):
+        if self.current < stop: 
+            next_time = '{0:02d}:{1:02d}:{2:02d}'.format(self.current // 60 // 60 % 24, self.current // 60 % 60, self.current % 60 % 60)
+            self.current += 1
+            return next_time
         else:
-            raise IndexError
+            raise StopIteration
 
-    def __getimte__():
+    def __getitem__(self, index):
         if index < self.stop - self.start:
-            
+            index_time = self.start + index
+            return '{0:02d}:{1:02d}:{2:02d}'.format(index_time // 60 // 60 % 24, index_time // 60 % 60, index_time % 60 % 60)
         else:
             raise IndexError
 
