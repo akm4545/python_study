914697443ed1ad8ea849d2c547e8df7abc4476a3
akm4545 <akm4545@naver.com>
2023-08-28 10:52:01 +0900

    unit45

---
+++ unit45/calcpkg/__ini__.py
@@ -0,0 +1,4 @@
+__all__ = ['add', 'triangle_area']    # calcpkg 패키지에서 add, triangle_area 함수만 공개
+
+from .operation import *
+from .geometry import * 
--- unit45/main.py
+++ unit45/main.py
@@ -1,4 +1,7 @@
-import person
-
-maria = person.Person('마리아', 20, '서울시 서초구 반포동')
-maria.greeting()
+import calcpkg
+ 
+print(calcpkg.add(10, 20))
+print(calcpkg.mul(10, 20))
+ 
+print(calcpkg.triangle_area(30, 40))
+print(calcpkg.rectangle_area(30, 40))
